80e44be8bee1f765b1874ffc5aca93f8f56a102a
Aayush Gour <138443486+itsaayush27@users.noreply.github.com>
2025-08-31 15:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	 if (!s2) return 0; // empty substring always at index 0
+  return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
+
